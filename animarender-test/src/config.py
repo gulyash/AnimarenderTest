133bb51ca4af7ed496b8df92895662511fe7b295
gulnara <igulnara6@gmail.com>
2017-11-06 00:17:12 +0500

    config

--- animarender-test/src/config.py
+++ animarender-test/src/config.py
@@ -1,14 +1,14 @@
 DATABASE_CONFIG = {
-    'host': <host here>,
-    'port': <port here>,
-    'username': <username here>,
-    'password': <password here>,
-    'database': <database here>,
+    'host': 'localhost',
+    'port': '3306',
+    'username': 'gulnara',
+    'password': 'pol',
+    'database': 'test_db',
 }
 
 RABBITMQ_CONFIG = {
-    'host': <host here>,
-    'port': <port here>,
-    'username': <username here>,
-    'password': <password here>,
+    'host': 'localhost',
+    'port': 5672,
+    'username': 'gulnara',
+    'password': 'derp',
 }
